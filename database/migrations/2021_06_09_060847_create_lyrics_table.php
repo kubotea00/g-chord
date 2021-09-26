53df3d1e9eeb62313a2d6c3163902323e13c3b1f
kubotea00 <kubotea00@gmail.com>
2021-09-26 23:10:20 +0900

    [update] 画像の表示方法を変更しました。

--- database/migrations/2021_06_09_060847_create_lyrics_table.php
+++ database/migrations/2021_06_09_060847_create_lyrics_table.php
@@ -17,6 +17,7 @@ class CreateLyricsTable extends Migration
             $table->bigIncrements('id');
             $table->bigInteger('song_title_id');
             $table->string('name');
+            $table->string('url');
             $table->timestamps();
         });
     }
