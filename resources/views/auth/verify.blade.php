d2c9e72bb7c38a750cec5b57e853749be5bb78e6
kubotea00 <kubotea00@gmail.com>
2021-09-26 21:52:34 +0900

    [update] ログイン周りの表記等を日本語に変更しました。

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,20 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('メールアドレスを確認する') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('新しい認証リンクがあなたのメールアドレスに送信されました。') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('続ける前に送信された認証リンクをご確認ください') }}
+                    {{ __('メールが届かない場合l') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('ここをクリックしてもう一度リクエストしてください。') }}</button>.
                     </form>
                 </div>
             </div>
